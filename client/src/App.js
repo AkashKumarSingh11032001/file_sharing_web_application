fc8318075b06e0fcaf4f29a2841cf1047b8bc185
Akash Singh <akashkumarsingh11032001@gmail.com>
2023-10-12 08:12:53 +0530

    frontend design

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  const logo = "https://i.pinimg.com/originals/16/46/24/1646243661201a0892cc4b1a64fcbacf.jpg"
+  const logo =
+    "https://i.pinimg.com/originals/16/46/24/1646243661201a0892cc4b1a64fcbacf.jpg";
   return (
-    <div className='container'>
-      <img src={logo} alt="logo" />
-      <div></div>
+    <div className="container">
+      <img src={logo} alt="banner" />
+      <div className="wrapper">
+        <h1>Simple file sharing!</h1>
+        <p>Upload and share the download link.</p>
+
+        <button>Upload</button>
+      </div>
     </div>
   );
 }
