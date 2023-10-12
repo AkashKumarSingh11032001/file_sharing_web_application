12b9439c28e7f8afeb8f28a3c4835f56afb018f2
Akash Singh <akashkumarsingh11032001@gmail.com>
2023-10-12 08:29:11 +0530

    api func

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
-import { useState, useEffect, useRef } from 'react';
-import './App.css';
-// import { uploadFile } from './service/api';
+import { useState, useEffect, useRef } from "react";
+import "./App.css";
+import { uploadFile } from './service/api';
 
 function App() {
+  const [file, setFile] = useState("");
+  const [result, setResult] = useState("");
+
   const logo =
     "https://i.pinimg.com/originals/16/46/24/1646243661201a0892cc4b1a64fcbacf.jpg";
 
@@ -10,6 +13,20 @@ function App() {
   const onUploadClick = () => {
     fileInputRef.current.click();
   };
+
+  useEffect(() => {
+    const getImage = async () => {
+      if (file) {
+        const data = new FormData();
+        data.append("name", file.name);
+        data.append("file", file);
+
+        const response = await uploadFile(data);
+        setResult(response.path);
+      }
+    };
+    getImage();
+  }, [file]);
 
   return (
     <div className="container">
@@ -23,7 +40,7 @@ function App() {
           type="file"
           ref={fileInputRef}
           style={{ display: "none" }}
-          // onChange={(e) => setFile(e.target.files[0])}
+          onChange={(e) => setFile(e.target.files[0])}
         />
       </div>
     </div>
