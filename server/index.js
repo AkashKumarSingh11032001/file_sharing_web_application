981c8c99ae3eef2249cfd275a8d05f1785771b06
Akash Singh <akashkumarsingh11032001@gmail.com>
2023-10-13 07:54:08 +0530

    uploadimages

--- server/index.js
+++ server/index.js
@@ -1,5 +1,9 @@
-import express from 'express'
+import express from "express";
+import router from "./routes/routes";
 
 const app = express();
+app.use("/", router);
 const PORT = 8000;
-app.listen(PORT,()=>{console.log("Server up and running in port => 8000")})
+app.listen(PORT, () => {
+  console.log("Server up and running in port => 8000");
+});
